36c5aa2211d5825737be2756658bd29ff2c1d0a5
Barcollin Jay <jubilates@hotmail.com>
2023-01-25 07:55:51 +0000

    added WriteJSON method

--- tools.go
+++ tools.go
@@ -207,6 +207,7 @@ type JSONResponse struct {
 	Data    interface{} `json:"data,omitempty"`
 }
 
+// ReadJSON tries to read the body of a request and converts from json to a go data variable
 func (t *Tools) ReadJSON(w http.ResponseWriter, r *http.Request, data interface{}) error {
 	maxBytes := 1024 * 1024
 	if t.MaxJSONSize != 0 {
@@ -265,4 +266,28 @@ func (t *Tools) ReadJSON(w http.ResponseWriter, r *http.Request, data interface{
 
 	return nil
 
+}
+
+// WriteJSON takes a response status code and arbitrary data and writes json to the client.
+func (t *Tools) WriteJSON(w http.ResponseWriter, status int, data interface{}, headers ...http.Header) error {
+	out, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for key, value := range headers[0] {
+			w.Header()[key] = value
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_, err = w.Write(out)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
